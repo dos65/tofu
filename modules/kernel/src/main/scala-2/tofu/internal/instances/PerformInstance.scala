bb535d0e70b993780717157cba5788a7262b88ed
Vladislav Ugryumov <vlad.ugrum@gmail.com>
2023-11-07 11:11:49 +0300

    Clean up.

--- modules/kernel/src/main/scala-2/tofu/internal/instances/PerformInstance.scala
+++ modules/kernel/src/main/scala-2/tofu/internal/instances/PerformInstance.scala
@@ -4,14 +4,14 @@ package instances
 import tofu.internal.carriers.{PerformCarrier2, PerformCarrier2Context, PerformCarrier3}
 import tofu.kernel.types.PerformThrow
 
-trait PerformInstance  extends PerformInstance1 {
+private[tofu] trait PerformInstance  extends PerformInstance1 {
   final implicit def interopCE3[F[_]](implicit carrier: PerformCarrier3[F]): PerformThrow[F] = carrier
 }
-trait PerformInstance1 extends PerformInstance2 {
+private[tofu] trait PerformInstance1 extends PerformInstance2 {
   final implicit def interopCE2[F[_]](implicit carrier: PerformCarrier2[F]): PerformThrow[F] = carrier
 }
 
-trait PerformInstance2 {
+private[tofu] trait PerformInstance2 {
   final implicit def interopCE2Contextual[F[_]](implicit
       carrier: PerformCarrier2Context[F]
   ): PerformThrow[F] = carrier
